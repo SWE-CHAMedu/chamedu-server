f0a11b1390c639a0d4582c103ad61782f1d831be
KimMinHwan <127469367+rlaalsghks8@users.noreply.github.com>
2023-11-07 16:09:26 +0900

    ChamEdu 프로젝트 생성

---
+++ ChamEdu_v1/src/test/java/com/example/chamedu_v1/ChamEduV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chamedu_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamEduV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
