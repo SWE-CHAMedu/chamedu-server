ba68c1b3f0ca33eeffdac40672514b385ba4a94e
Semin Kim <101847670+semnisem@users.noreply.github.com>
2023-11-26 22:12:39 +0900

    회원가입 및 로그인 요청 dto 생성

---
+++ ChamEdu_v1/src/main/java/com/example/chamedu_v1/data/dto/MemberSignInRequestDto.java
@@ -0,0 +1,24 @@
+package com.example.chamedu_v1.data.dto;
+
+public class MemberSignInRequestDto {
+    private String username;
+    private String password;
+
+    // Getter and Setter methods
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
