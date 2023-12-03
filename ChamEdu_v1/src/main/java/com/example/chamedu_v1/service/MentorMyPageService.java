04155e1bf49d52e88cdd7042685c14db2845a33e
Semin Kim <101847670+semnisem@users.noreply.github.com>
2023-12-03 23:10:11 +0900

    상담 요청 및 예정 목록 정보추가

--- ChamEdu_v1/src/main/java/com/example/chamedu_v1/service/MentorMyPageService.java
+++ ChamEdu_v1/src/main/java/com/example/chamedu_v1/service/MentorMyPageService.java
@@ -1,5 +1,6 @@
 package com.example.chamedu_v1.service;
-
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import com.example.chamedu_v1.data.dto.*;
 import com.example.chamedu_v1.data.entity.*;
 import com.example.chamedu_v1.data.repository.*;
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.temporal.Temporal;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 @Component
@@ -31,19 +33,32 @@ public class MentorMyPageService {
     private ReviewRepository reviewRepository;
     @Autowired
     private RoomRepository roomRepository;
-
     @Autowired
     private MenteeRepository menteeRepository;
 
-    // 상담신청 목록 조회
-    public List<Room> receiveChatRequests(String userId) {
-        return roomRepository.findAllByMentor_UserIdAndStatus(userId, 'W');
+    // 상담목록 조회 ('W'는 신청목록, 'A'는 예정목록)
+    public List<ChatInfoDto> checkChatRequests(String userId, char status) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.KOREA);
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm", Locale.KOREA);
+        List<Room> rooms= roomRepository.findAllByMentor_UserIdAndStatus(userId, status);
+        return rooms.stream()
+                .map(room -> {
+                    String dateString = dateFormat.format(room.getStartDate());
+                    String startTimeString = timeFormat.format(room.getStartDate());
+                    String endTimeString = timeFormat.format(room.getEndDate());
+                    return new ChatInfoDto(
+                            room.getRoomId(),
+                            room.getMentee().getName(),
+                            dateString,
+                            startTimeString+"~"+endTimeString,
+                            room.getChatTitle()
+                    );
+                })
+                .toList();
     }
 
-    // 상담예정 목록 조회
-    public List<Room> plannedChatRequests(String userId) {
-        return roomRepository.findAllByMentor_UserIdAndStatus(userId, 'A');
-    }
+
+
     // 상담 수락 또는 거절 처리
     @Transactional
     public void answerChatRequests(ChatAnswerRequestDto chatAnswerRequestDto) {
